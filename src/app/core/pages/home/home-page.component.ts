b37fb827a080c37d61cc918f02ce45c1f1104e17
Victor Zapata <victorzapatabedoya@hotmail.com>
2025-10-09 09:54:54 -0500

    Restore previous home page component

--- src/app/core/pages/home/home-page.component.ts
+++ src/app/core/pages/home/home-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { OAuthService } from 'angular-oauth2-oidc';
-import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home-page',
@@ -9,14 +8,9 @@ import {HttpClient} from '@angular/common/http';
 })
 export class HomePageComponent implements OnInit {
   private oAuthService = inject(OAuthService);
-  private httpClient = inject(HttpClient);
 
   ngOnInit(): void {
     console.log('Access token: ' + this.oAuthService.getAccessToken());
     console.log('Identity claims: ', this.oAuthService.getIdentityClaims());
-    this.httpClient.get("https://localhost:8080/api/test/protected").subscribe({
-      next: (data) => console.log("Protected data: ", data),
-      error: (error) => console.error("Error fetching protected data: ", error)
-    });
   }
 }
