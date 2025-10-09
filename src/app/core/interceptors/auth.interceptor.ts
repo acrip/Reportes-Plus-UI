d2be066780a4060ad6181ed8a59812e527ae1aa4
Victor Zapata <victorzapatabedoya@hotmail.com>
2025-10-09 09:50:12 -0500

    Remove token service

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,18 +1,17 @@
-import { HttpContextToken, HttpInterceptorFn } from '@angular/common/http';
-import { inject } from '@angular/core';
-import { TokenService } from '../services/token.service';
+import {HttpContextToken, HttpInterceptorFn} from '@angular/common/http';
+import {inject} from '@angular/core';
+import {OAuthService} from 'angular-oauth2-oidc';
 
 // Contexto para saltar el header Authorization en peticiones puntuales
 export const SKIP_AUTH = new HttpContextToken<boolean>(() => false);
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(OAuthService);
+
   // ¿Esta petición debe omitir el auth?
   if (req.context.get(SKIP_AUTH)) {
     return next(req);
   }
-
-  const tokenService = inject(TokenService);
-  const token = tokenService.getToken();
 
   // Asegura headers comunes
   let headers = req.headers.set('Accept', 'application/json');
@@ -23,8 +22,9 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   }
 
   // Agrega Authorization si hay token
-  if (token) {
-    headers = headers.set('Authorization', `Bearer ${token}`);
+  if (authService.hasValidAccessToken()) {
+    console.warn("Has valid access token")
+    headers = headers.set('Authorization', `Bearer ${authService.getAccessToken()}`);
   }
 
   const authReq = req.clone({ headers });
